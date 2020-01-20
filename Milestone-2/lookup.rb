e7676439507fbcfe26ac7cf46eb73c316c0d8ea7
ChowtooriKedari <chowtoorikedari@gmail.com>
2020-01-20 18:47:43 +0530

    Milestone-2

--- Milestone-2/lookup.rb
+++ Milestone-2/lookup.rb
@@ -18,51 +18,49 @@ domain = get_command_line_argument
 # https://www.rubydoc.info/stdlib/core/IO:readlines
 dns_raw = File.readlines("zone")
 
-
 def parse_dns(dns_raw)
-j=0
-dns_raw=dns_raw.select{ |i| i!="\n"}
-dns_raw=dns_raw.select{ |i| !(i.start_with?"#")}
-dns_records=[]
-dns_raw.each do |i|
-m=i.split(',')
-m[1]=m[1].strip
-m[2]=m[2].strip
-dns_records[j] = {
-   :d1=>m[0],
-   :d2=>m[1],
-   :d3=>m[2],
-}
-j=j+1
+  increment_value = 0
+  dns_raw = dns_raw.select { |record| record != "\n" }
+  dns_raw = dns_raw.select { |record| !(record.start_with? "#") }
+  dns_records = []
+  dns_raw.each do |record|
+    message = record.split(",")
+    message[1] = message[1].strip
+    message[2] = message[2].strip
+    dns_records[increment_value] = {
+      :field1 => message[0],
+      :field2 => message[1],
+      :field3 => message[2],
+    }
+    increment_value = increment_value + 1
+  end
+  return dns_records
 end
-return dns_records
-end
-
 
 def resolve(dns_records, lookup_chain, domain)
-l=dns_records.length
-c=0
-dns_records.each do |records|
- if records[:d2]==domain
-     if records[:d1]=="A"
-     lookup_chain.push(records[:d3])
-     elsif records[:d1]="CNAME"
-     lookup_chain.push(records[:d3])
-     resolve(dns_records,lookup_chain,records[:d3])
-     end
- elsif records[:d2]!=domain
-          c=c+1
-          if c==l
-            lookup_chain=[]
-            s="record not found for " +domain
-	    lookup_chain.push(s)
-          end
- end
-end
-return lookup_chain
+  dns_length = dns_records.length
+  record_count = 0
+  dns_records.each do |records|
+    if records[:field2] == domain
+      if records[:field1] == "A"
+        lookup_chain.push(records[:field3])
+      elsif records[:field1] = "CNAME"
+        lookup_chain.push(records[:field3])
+        resolve(dns_records, lookup_chain, records[:field3])
+      end
+    elsif records[:d2] != domain
+      record_count = record_count + 1
+      if record_count == dns_length
+        lookup_chain = []
+        string_value = "Error: record not found for " + domain
+        lookup_chain.push(string_value)
+      end
+    end
+  end
+  return lookup_chain
 end
 
-dns_records=parse_dns(dns_raw)
+dns_records = parse_dns(dns_raw)
 lookup_chain = [domain]
 lookup_chain = resolve(dns_records, lookup_chain, domain)
 puts lookup_chain.join(" => ")
